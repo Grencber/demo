2f5ebddcf130d58f42e813924e58eee880c0810f
Gökalp Rençber <grencber@gmail.com>
2021-06-18 21:56:15 +0300

    testInitialDatabaseTransactionService is added

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,22 +2,33 @@ package com.example.demo;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.mockitoSession;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
+import java.util.List;
+
+import javax.annotation.Resource;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
+import com.example.demo.domain.AccountDataMapper;
+import com.example.demo.domain.CustomerDataMapper;
 import com.example.demo.exceptions.CustomerInputNotValidException;
 import com.example.demo.exceptions.CustomerNotFoundException;
 import com.example.demo.interfaceAdapters.Controller;
 import com.example.demo.interfaceAdapters.CreateAccountRequestMapper;
 import com.example.demo.interfaceAdapters.JpaAccountRepository;
 import com.example.demo.interfaceAdapters.JpaCustomerRepository;
+import com.example.demo.services.AccountService;
+import com.example.demo.services.CustomerService;
+import com.example.demo.services.InitialDataTransactionService;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -30,6 +41,12 @@ class DemoApplicationTests {
 	
 	@Autowired
 	Controller controller;
+	
+	@Resource
+	private JpaCustomerRepository jpaCustomerRepository;
+	
+	@Resource
+	private JpaAccountRepository JpaAccountRepository;
 	
 	@Test
 	void testCreateAccountForExistingCustomerId() {
@@ -57,12 +74,14 @@ class DemoApplicationTests {
 		Throwable exception = assertThrows(CustomerInputNotValidException.class, () -> controller.createAccount(createAccountRequestObject));
 		assertEquals("CustomerId 0 is not valid...", exception.getMessage());
 	}
-//	@Test
-//	void testDB() {
-//		CustomerDataMapper customer = new CustomerDataMapper(1, "Emre", "Aytekin");
-//		customerRepo.save(customer);
-//		AccountDataMapper account = new AccountDataMapper(10, new BigInteger("100,000"));
-//		accountRepo.save(account);
-//	}
+	
+	
+	@Test
+	void testInitialDatabaseTransactionService() {
+		assertEquals("Emre", jpaCustomerRepository.findById(1).get().getName());
+		assertEquals("Psycho", jpaCustomerRepository.findById(1).get().getSurname());
+		assertEquals("1000.00", JpaAccountRepository.findById(2).get().getBalance().toPlainString());
+	}
+	
 
 }
